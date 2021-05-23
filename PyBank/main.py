05b1aaa36a92807a5db366ba7b91f822aa859047
Antler_Zutter <xiaokunyu@macbook-pro.home>
2021-05-23 22:28:06 +1000

    Finish Pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,38 @@
-print
+import os
+import csv
+from typing import Counter
+
+budget_csv = os.path.join("Resources","PyBank_budget_data.csv")
+
+with open(budget_csv) as csvfile:
+    csvreader = csv.reader(csvfile,delimiter=",")
+    next(csvreader,None)
+    max_row = 0
+    total_a = 0
+    revenue = []
+    date = []
+    ave_rev = []
+    #figure out the total row number
+    for row in csvreader:
+        if row[0] is not None:
+            revenue.append(float(row[1]))
+            date.append(row[0])
+            max_row += 1
+            total_a = total_a + int(row[1])
+    print("Financial Analysis")
+    print("-------------------------------")
+    print("Total Months: " + str(max_row))
+    print("Total: $" + str(total_a))
+
+    for i in range(1,max_row):
+        ave_rev.append(revenue[i] - revenue[i-1])
+        arc = sum(ave_rev)/len(ave_rev)
+   
+        max_rev = max(ave_rev)
+        min_rev = min(ave_rev)
+        max_date = date[ave_rev.index(max_rev)+1]
+        min_date = date[ave_rev.index(min_rev)+1]
+    
+    print("Average Change: $" + "%.2f" % arc)
+    print("Greatest Increase in Revenue: " + str(max_date) + " ($" + "%.0f" % max_rev + ")")
+    print("Greatest Decrease in Revenue: " + str(min_date) + " ($" + "%.0f" % min_rev + ")")
